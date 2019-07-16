7a18b7597567f71aaf42fe32318a5ec1a777c60e
valenlyn <valenlynchua@gmail.com>
2019-07-16 23:49:41 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import Auth from '@aws-amplify/auth';
+import awsconfig from './aws-exports';
+
+// retrieve temporary AWS credentials and sign requests
+Auth.configure(awsconfig);
+
 function App() {
     return(
         <h1>hello world</h1>
